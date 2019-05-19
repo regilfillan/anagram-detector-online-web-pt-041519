9ffe6d7ba707a88029e42ea65905ab3110c56ec8
Ryan Gilfillan <regilfillan@gmail.com>
2019-05-19 21:09:03 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,10 +2,10 @@ class Anagram
   attr_accessor :word 
   
   def initialize(word)
-    if @word.match?(array)
+    if @word.match(%w(array))
       array[word]
     else
-      []
+      Array.new
   end 
   
 end 
